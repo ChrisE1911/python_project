d45214fa8792abd1a4c2d0e311138689d1b86f95
Ki Woo Kim <kwedgemusic@gmail.com>
2023-02-10 14:20:01 -0800

    seeds folder import statements added

--- practice-for-week-19-python-project-skeleton/app/models/__init__.py
+++ practice-for-week-19-python-project-skeleton/app/models/__init__.py
@@ -1,3 +1,9 @@
 from .db import db
 from .user import User
+from .answer import Answer
+from .dislike import Dislike
+from .like import Like
+from .picture import Picture
+from .profile import Profile
+from .question import Question
 from .db import environment, SCHEMA
---
+++ practice-for-week-19-python-project-skeleton/app/seeds/profiles.py
@@ -0,0 +1,1 @@
+from app.models import db, Profile, environment, SCHEMA

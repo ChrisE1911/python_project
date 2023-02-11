f599f287ed9fe7a06895483a3aa464b6ae978d8d
Ki Woo Kim <kwedgemusic@gmail.com>
2023-02-11 15:21:24 -0800

    order seeds

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -23,12 +23,12 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
+        undo_matches()
+        undo_likes()
         undo_answers()
         undo_pictures()
         undo_profiles()
         undo_questions()
-        undo_matches()
-        undo_likes()
         undo_users()
 
     users = seed_users()
@@ -45,11 +45,11 @@ def seed():
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_matches()
+    undo_likes()
     undo_answers()
     undo_pictures()
     undo_profiles()
     undo_questions()
-    undo_matches()
-    undo_likes()
     undo_users()
     # Add other undo functions here

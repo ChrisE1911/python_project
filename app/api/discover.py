16a05395b9d92ea8b9706af48619eb559393dcc3
Ki Woo Kim <kwedgemusic@gmail.com>
2023-02-12 17:06:47 -0800

    backendroute /discover working

--- app/api/discover.py
+++ app/api/discover.py
@@ -1,16 +1,28 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required, current_user
-from app.models import User, Profile, Picture
+from app.models import User, Profile, Picture, Answer, likes
 
-discover_routes = Blueprint('discover',__name__)
+
+discover_routes = Blueprint('discover', __name__)
+
 
 @discover_routes.route('/')
-# @login_required
+@login_required
 def queue():
     """
     Query for all users, filtering out current user and those the user has already liked/disliked
     """
-    # self_id = current_user(id)
-    # users = User.query.all().filter()
-    print(current_user)
-    return current_user
+    self_id = current_user.id
+    user = User.query.get(self_id)
+    likes = user.like_requests.all()
+    likes_dicted = [ele.to_dict() for ele in likes]
+    likes_id = [like['id'] for like in likes_dicted]
+
+    all_users = User.query.filter(User.id != self_id).all()
+    unliked_users = [user.to_dict()
+                     for user in all_users if user.id not in likes_id]
+    print('PRINTTTTTT', all_users)
+    # return jsonify({"filtered": [ele.to_dict() for ele in filtered]})
+    return {
+        'unliked_users': unliked_users
+    }

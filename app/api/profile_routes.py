73be5467aeebb104d0dbed43ca4f4e3d973c9de8
Ki Woo Kim <kwedgemusic@gmail.com>
2023-02-15 12:21:16 -0800

    old state issues

--- app/api/profile_routes.py
+++ app/api/profile_routes.py
@@ -27,28 +27,28 @@ def create_profile():
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
         user_profile = Profile(
-          user_id=current_user.id,
-          city=form.data['city'],
-          state=form.data['state'],
-          occupation=form.data['occupation'],
-          gender=form.data['gender'],
-          sexual_orientation=form.data['sexual_orientation'],
-          height=form.data['height'],
-          religion=form.data['religion'],
-          political_affiliation=form.data['political_affiliation'],
-          language=form.data['language'],
-          kids=form.data['kids'],
-          pets=form.data['pets'],
-          drinker=form.data['drinker'],
-          diet=form.data['diet'],
-          smoker=form.data['smoker'],
-          marijuana=form.data['marijuana'],
-          zodiac=form.data['zodiac'],
-          ethnicity=form.data['ethnicity'],
-          body_type=form.data['body_type'],
-          education_level=form.data['education_level'],
-          bio=form.data['bio'],
-          age=form.data['age']
+            user_id=current_user.id,
+            city=form.data['city'],
+            state=form.data['state'],
+            occupation=form.data['occupation'],
+            gender=form.data['gender'],
+            sexual_orientation=form.data['sexual_orientation'],
+            height=form.data['height'],
+            religion=form.data['religion'],
+            political_affiliation=form.data['political_affiliation'],
+            language=form.data['language'],
+            kids=form.data['kids'],
+            pets=form.data['pets'],
+            drinker=form.data['drinker'],
+            diet=form.data['diet'],
+            smoker=form.data['smoker'],
+            marijuana=form.data['marijuana'],
+            zodiac=form.data['zodiac'],
+            ethnicity=form.data['ethnicity'],
+            body_type=form.data['body_type'],
+            education_level=form.data['education_level'],
+            bio=form.data['bio'],
+            age=form.data['age']
         )
         # user_picture = Picture(
         #   user_id=current_user.id,
@@ -61,6 +61,7 @@ def create_profile():
         print('HELLO', user_profile)
         return user_profile.to_dict()
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+
 
 @profile_routes.route('/current_user')
 @login_required
@@ -81,30 +82,45 @@ def edit_profile():
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
         edited_profile = Profile(
-          user_id=current_user.id,
-          city=form.data['city'],
-          state=form.data['state'],
-          occupation=form.data['occupation'],
-          gender=form.data['gender'],
-          sexual_orientation=form.data['sexual_orientation'],
-          height=form.data['height'],
-          religion=form.data['religion'],
-          political_affiliation=form.data['political_affiliation'],
-          language=form.data['language'],
-          kids=form.data['kids'],
-          pets=form.data['pets'],
-          diet=form.data['diet'],
-          smoker=form.data['smoker'],
-          marijuana=form.data['marijuana'],
-          zodiac=form.data['zodiac'],
-          ethnicity=form.data['ethnicity'],
-          body_type=form.data['body_type'],
-          education_level=form.data['education_level'],
-          bio=form.data['bio'],
-          age=form.data['age']
+            user_id=current_user.id,
+            city=form.data['city'],
+            state=form.data['state'],
+            occupation=form.data['occupation'],
+            gender=form.data['gender'],
+            sexual_orientation=form.data['sexual_orientation'],
+            height=form.data['height'],
+            religion=form.data['religion'],
+            political_affiliation=form.data['political_affiliation'],
+            language=form.data['language'],
+            kids=form.data['kids'],
+            pets=form.data['pets'],
+            diet=form.data['diet'],
+            smoker=form.data['smoker'],
+            marijuana=form.data['marijuana'],
+            zodiac=form.data['zodiac'],
+            ethnicity=form.data['ethnicity'],
+            body_type=form.data['body_type'],
+            education_level=form.data['education_level'],
+            bio=form.data['bio'],
+            age=form.data['age']
         )
+
         db.session.add(edited_profile)
         # db.session.add(user_picture)
         db.session.commit()
         print('EDITED PROFILE!!!!!', edited_profile.to_dict())
         return edited_profile.to_dict()
+
+
+# UPDATING A RECORD
+# selected_user = User.query.get(form.data["user"])
+# selected_user.username = "Bradley"
+# db.session.commit()
+
+        # to_update_profile = Profile.query.filter(
+        #     Profile.user_id == current_user.id).first()
+        # to_update_profile = edited_profile
+        # db.session.add(to_update_profile)
+        # db.session.commit()
+        # print('INSIDE FLASK ROUTE-----> ,', to_update_profile.to_dict())
+        # return to_update_profile.to_dict()

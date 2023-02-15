cc53f44a3cfedbb53331f4a04b270334c966197d
Sean <seankim912@yahoo.com>
2023-02-14 22:58:13 -0600

    current user profile working

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -1,6 +1,5 @@
 import Discover from "../Discover";
 import "./HomePage.css";
-import { NavLink } from "react-router-dom";
 
 function HomePage() {
 	return (
@@ -9,9 +8,6 @@ function HomePage() {
 				<li>Discover (NAVIGATION)</li>
 				<li>Likes</li>
 				<li>Questions</li>
-				<NavLink exact to="/profile/current_user">
-					<button>My Profile</button>
-				</NavLink>
 			</nav>
 			<Discover />
 		</>

4399a44eda561415eec01899f8e43d9c57f01ddd
Ki Woo Kim <kwedgemusic@gmail.com>
2023-02-17 11:45:23 -0800

    discover page image scroll working

--- react-app/src/components/ProfileCard/index.js
+++ react-app/src/components/ProfileCard/index.js
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import { thunkCreateLike, thunkCreateDislike } from "../../store/like";
-
+import "./Discover.css";
 export default function ProfileCard({ user, updateUserNumber }) {
 	const admirer_id = useSelector((state) => state.session.user);
 	const hater_id = useSelector((state) => state.session.user);
@@ -22,13 +22,16 @@ export default function ProfileCard({ user, updateUserNumber }) {
 		updateUserNumber();
 		dispatch(thunkCreateDislike(hate_receiver_id, hater_id));
 	}
-	
+	function isImageLengthOne() {
+		return user.profile.userImages.length !== 1;
+	}
+
 	console.log(user.profile);
 	return (
 		<div className='discover_container'>
 			<div className='discover_center_container'>
 				<div className='top_container'>
-					<div>
+					<div className='name-btn-container'>
 						<div className='left_div'>
 							<h1>{user.firstname}</h1>
 							<span>{user.profile.age}</span>
@@ -41,10 +44,26 @@ export default function ProfileCard({ user, updateUserNumber }) {
 						</div>
 					</div>
 					<div className='discover_image'>
-						<img
-							src={user.profile.userImages[0].picture_url}
-							alt='profile-pic'
-						/>
+						{/* {isImageLengthOne ? (
+							<img
+								src={user.profile.userImages[0].picture_url}
+								alt='profile-pic'
+							/>
+						) : (
+							user.profile.userImages.map((img) => (
+								<div key={img.id} className='profile-img-container'>
+									<img src={img.picture_url} />
+								</div>
+							))
+						)}
+
+
+						*/}
+						{user.profile.userImages.map((img) => (
+							<div key={img.id} className='profile-img-container'>
+								<img src={img.picture_url} />
+							</div>
+						))}
 					</div>
 				</div>
 				<div className='bottom-container'>
@@ -53,14 +72,35 @@ export default function ProfileCard({ user, updateUserNumber }) {
 						<p>{user.profile.bio}</p>
 					</div>
 					<div className='details'>
-						<h5>{user.profile.sexual_orientation}</h5>
-						<h5>{user.profile.height}</h5>
-						<h5>{user.profile.body_type}</h5>
-						<h5>{user.profile.ethnicity}</h5>
-						<h5>{user.profile.language}</h5>
+						<h5>{`${user.profile.gender}  |  ${user.profile.sexual_orientation}`}</h5>
+						<h5>{`${user.profile.height}  |  ${user.profile.body_type}`}</h5>
+						<h5>{`${user.profile.ethnicity} | ${user.profile.political_affiliation} | ${user.profile.language} | ${user.profile.education_level} | ${user.profile.occupation} | ${user.profile.religion} | ${user.profile.zodiac}`}</h5>
+						<h5>{`${user.profile.smoker}  |  ${user.profile.drinker} | ${user.profile.marijuana} | ${user.profile.diet}`}</h5>
+						<h5>{`${user.profile.kids} | ${user.profile.pets} `}</h5>
 					</div>
 				</div>
 			</div>
 		</div>
 	);
+}
+
+{
+	/* <h5>{user.profile.height}</h5>
+<h5>{user.profile.body_type}</h5>
+<h5>{user.profile.ethnicity}</h5>
+<h5>{user.profile.language}</h5>
+<h5>{user.profile.religion}</h5>
+<h5>{user.profile.political_affiliation}</h5>
+<h5>{user.profile.kids}</h5>
+<h5>{user.profile.pets}</h5>
+<h5>{user.profile.diet}</h5>
+<h5>{user.profile.smoker}</h5>
+<h5>{user.profile.drinker}</h5>
+<h5>{user.profile.marijuana}</h5>
+<h5>{user.profile.zodiac}</h5>
+{/* <h5>{user.profile.gender}</h5> */
+}
+{
+	/* <h5>{user.profile.occupation}</h5>
+<h5>{user.profile.education_level}</h5> */
 }

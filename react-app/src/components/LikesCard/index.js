3db9f82e0d1cbcce6e76c4288365bf3321fe3edc
Sean <seankim912@yahoo.com>
2023-02-17 15:35:01 -0600

    basic likes css

--- react-app/src/components/LikesCard/index.js
+++ react-app/src/components/LikesCard/index.js
@@ -19,13 +19,15 @@ const LikeCard = ({ like }) => {
 
     console.log('BBBBBB', like)
     return (
-        <div id="likes-card">
+        <div className="likes-card">
         {/* trying to add image from profile state */}
-            <div>
+            <div className="likes-image">
                 <img src={like.profile.userImages[0].picture_url} style={{width: "100px", height: '100px'}}></img>
             </div>
             <div>
                 {`${like.firstname}, ${like.profile.age}`}
+            </div>
+            <div>
                 <div>{`${like.profile.city}, ${like.profile.state}`}</div>
             </div>
 {/* Add onclick and handle delete like to this component */}
